6d4eb29d162d581490db0e2334b52502279ec457
biryang <hwang7531@gmail.com>
2021-02-02 18:20:47 +0900

    홈 스크린 추가

---
+++ start_flutter/flutter_start_app/lib/screen/home_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_start_app/model/model_movie.dart';
+import 'package:flutter_start_app/screen/carousel_slider.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Movie> movies = [
+    //영화 더미 데이터
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'test_movie_1.png',
+      'like': false
+    })
+  ];
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        //캐로셀 이미지가 하단에 쌓이고 위에 탑바가 쌓임 순서대로 쌓임
+        Stack(
+          children: <Widget>[
+            CarouselImage(
+              moives: movies,
+            ),
+            TopBar(),
+          ],
+        )
+      ],
+    );
+  }
+}
+
+// 상단탭
+class TopBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
+      child: Row(
+        //균등 정렬
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          //에셋 이미지 호출
+          Image.asset(
+            'images/bbongflix_logo.png',
+            fit: BoxFit.contain,
+            height: 25,
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              'TV 프로그램',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '영화',
+              style: TextStyle(fontSize: 14),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(right: 1),
+            child: Text(
+              '내가 찜한 콘텐츠',
+              style: TextStyle(fontSize: 14),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
