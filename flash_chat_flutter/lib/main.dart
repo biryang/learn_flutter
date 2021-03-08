7819121e004814fd33f0a9370b270300d4db28d9
biryang <56025411+biryang@users.noreply.github.com>
2021-03-09 00:53:38 +0900

    Hero Animation

--- flash_chat_flutter/lib/main.dart
+++ flash_chat_flutter/lib/main.dart
@@ -15,12 +15,12 @@ class FlashChat extends StatelessWidget {
           bodyText2: TextStyle(color: Colors.black54),
         ),
       ),
-      initialRoute: '/',
+      initialRoute: WelcomeScreen.id,
       routes: {
-        '/':(context) => WelcomeScreen(),
-        '/login' : (context) => LoginScreen(),
-        '/registration' : (context) => RegistrationScreen(),
-        '/chat' : (context)=>ChatScreen()
+        WelcomeScreen.id:(context) => WelcomeScreen(),
+        LoginScreen.id : (context) => LoginScreen(),
+        RegistrationScreen.id : (context) => RegistrationScreen(),
+        ChatScreen.id : (context)=>ChatScreen()
       },
     );
   }
--- flash_chat_flutter/lib/screens/welcome_screen.dart
+++ flash_chat_flutter/lib/screens/welcome_screen.dart
@@ -1,7 +1,9 @@
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 
 class WelcomeScreen extends StatefulWidget {
-  String id = 'welcome';
+  static String id = 'welcome';
 
   @override
   _WelcomeScreenState createState() => _WelcomeScreenState();
@@ -20,9 +22,12 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
           children: <Widget>[
             Row(
               children: <Widget>[
-                Container(
-                  child: Image.asset('images/logo.png'),
-                  height: 60.0,
+                Hero(
+                  tag: 'logo',
+                  child: Container(
+                    child: Image.asset('images/logo.png'),
+                    height: 60.0,
+                  ),
                 ),
                 Text(
                   'Flash Chat',
@@ -45,6 +50,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to login screen.
+                    Navigator.pushNamed(context, LoginScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
@@ -63,6 +69,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
                 child: MaterialButton(
                   onPressed: () {
                     //Go to registration screen.
+                    Navigator.pushNamed(context, RegistrationScreen.id);
                   },
                   minWidth: 200.0,
                   height: 42.0,
