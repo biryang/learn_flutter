20edd80c3db205f20d540d340a2ebc70206262d8
biryang <56025411+biryang@users.noreply.github.com>
2021-04-19 00:15:02 +0900

    onSubmit통해 작성완료 버튼 확인시 추가

--- personal_expenses/lib/widgets/new_transaction.dart
+++ personal_expenses/lib/widgets/new_transaction.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
 class NewTransaction extends StatelessWidget {
-  String titleInput;
-  String amountInput;
-
+  final Function addTx;
   final titleController = TextEditingController();
   final amountController = TextEditingController();
+
+  NewTransaction(this.addTx);
+
+  void submitData() {
+    final enteredTitle = titleController.text;
+    final enteredAmount = double.parse(amountController.text);
+
+    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+      return;
+    }
+
+    addTx(
+      enteredTitle,
+      enteredAmount,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,23 +33,19 @@ class NewTransaction extends StatelessWidget {
             TextField(
               decoration: InputDecoration(labelText: 'Title'),
               controller: titleController,
-              onChanged: (val) {
-                titleInput = val;
-              },
             ),
             TextField(
               decoration: InputDecoration(labelText: 'Amount'),
               controller: amountController,
-              onChanged: (val) {
-                amountInput = val;
-              },
+              keyboardType: TextInputType.number,
+              onSubmitted: (_) => submitData(),
             ),
             TextButton(
-              onPressed: () {},
+              onPressed: submitData,
               child: Text('Add Transaction'),
               style: ButtonStyle(
                   foregroundColor:
-                  MaterialStateProperty.all<Color>(Colors.purple)),
+                      MaterialStateProperty.all<Color>(Colors.purple)),
             )
           ],
         ),
